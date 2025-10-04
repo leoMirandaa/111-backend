999e3e2649f1139307e1e2bee11c4bf5aafce3c5
Leopoldo Miranda <leomirandadev@gmail.com>
2025-10-04 12:05:16 -0700

    session#2

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import sqlite3
+from datetime import date
 
 app = Flask(__name__)
 
@@ -9,11 +10,26 @@ def init_db():
     conn = sqlite3.connect(DB_NAME) # Opens a connection to the database file named 'budget_manager.db'
     cursor = conn.cursor() # Creates a Cursor/Tool that lets us send commands(SELECT, INSERT...) to the database.
 
+    # Users Table
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       username TEXT UNIQUE NOT NULL,
       password TEXT NOT NULL              
+    )
+    """)
+
+    # Expenses Table
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS expenses (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT,
+      description TEXT NOT NULL,
+      amount INT NOT NULL,
+      date TEXT NOT NULL,
+      category TEXT NOT NULL,
+      user_id INTEGER,
+      FOREIGN KEY(user_id) REFERENCES users(id)
     )
     """)
 
@@ -61,6 +77,93 @@ def get_user(user_id):
     conn.close()
 
     return jsonify({"id": row["id"], "username": row["username"]}), 200
+
+
+# http://127.0.0.1:5000/api/users/2
+@app.put("/api/users/<int:user_id>") # Flask
+def update_user(user_id): # Python
+    data = request.get_json() # Flask
+    username = data.get("username") # Python
+    password = data.get("password") # Python
+
+    conn = sqlite3.connect(DB_NAME) # sqlite3
+    cursor = conn.cursor() # sqlite3
+
+    # Validate if user exists
+    cursor.execute("SELECT * from users where id=?", (user_id,)) # execute(), executes an SQL statement(SELECT,INSERT...)
+    if not cursor.fetchone(): #fetchone(), retrieves a single row from the result, useful when expecting only one record
+        conn.close()
+        return jsonify({'message': "User not found"}), 404
+
+    cursor.execute("UPDATE users SET username=?, password=? WHERE id=?", (username, password, user_id)) # SQL + sqlite3
+    conn.commit() # sqlite3
+    conn.close() # sqlite 3
+
+    return jsonify({"message": "User updated successfully"}), 200 # Flask
+
+
+# http://127.0.0.1:5000/api/users/2
+@app.delete("/api/users/<int:user_id>")
+def delete_user(user_id):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    # Validate if user exists
+    cursor.execute("SELECT * FROM users WHERE id=?", (user_id,)) # execute(), executes an SQL statement(SELECT,INSERT...)
+    if not cursor.fetchone(): #fetchone(), retrieves a single row from the result, useful when expecting only one record
+        conn.close()
+        return jsonify({'message': "User not found"}), 404
+
+
+    cursor.execute("DELETE FROM users WHERE id=?", (user_id,))
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "User deleted successfully"}), 200
+
+
+@app.get("/api/users")
+def get_users():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row # Allows column values to be retrieved by name, row["username"]
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall() # fetchall, retrieves all rows from the result of the query, returns a list of tuples
+    conn.close()
+
+    users = []
+    for row in rows:
+        user = {"id": row["id"], "username": row["username"], "password": row["password"]}
+        users.append(user)
+        
+    return jsonify(users), 200
+
+
+# ---------| EXPENSES |---------
+@app.post("/api/expenses")
+def create_expense():
+    data = request.get_json()
+    title = data.get("title")
+    description = data.get("description")
+    amount = data.get("amount")
+    category = data.get("category")
+    user_id = data.get("user_id")
+    date_str = date.today()
+
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+      INSERT INTO expenses (title, description, amount, date, category, user_id)
+      VALUES (?, ?, ?, ?, ?, ?)
+    """, (title, description, amount, date_str, category, user_id))
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Expense added successfully"}), 201
+
 
 
 if __name__ == "__main__":
